6ebcf6b5ee94a2675b5c9a2dba21251a8282f965
Jonathan Jan (RIT Student) <jmj2097@rit.edu>
2023-10-26 12:14:08 -0400

    began web apps exercise 5

--- 235/web-apps-lab/tmp.js
+++ 235/web-apps-lab/tmp.js
@@ -1,74 +1,14 @@
 "use strict";
-let colors = ["indigo","yellow","gray","purple","black"];
-let foods = ["penne vodka sauce", "reuben", "mac and cheese", "ramen", "sushi"]; // add some foods
 
-// Optional - can you figure out how to pull both the key AND the value
-// from the `links` object literal?
-let links = {
-    "RIT": "http://www.rit.edu",
-    "RWAG" : "https://www.facebook.com/RWAGclub",
-    "New Media Club" : "http://newmediaclub.cias.rit.edu"
-}   
+// Tips: onchange, onclick
+// attribute selector: input[name='colorGroup']
+// get all the things (legend, radios, btn, para)
 
+// JQuery was made by a cs student at RIT !?
+window.onload = init;
+// window.addEventListener("load", init); //Another way to do this
 
-// /**
-//  * I like doing this because specifying type based languages are better imo
-//  * @param {Element} listElement 
-//  * @param {Array} array 
-//  */
-// function createList(listElement, array = ["puppydogs", "butterflies", "lollipops"])
-// {
-//     for(let item of array)
-//     {
-//         const li = document.createElement("li");
-//         li.innerHTML = item;
-//         listElement.appendChild(li);
-//     }
-// }
-
-// /**
-//  * Using let
-//  * @param {Element} listElement 
-//  * @param {Array} array 
-//  */
-// let createList = function(listElement, array = ["puppydogs", "butterflies", "lollipops"])
-// {
-//     for(let item of array)
-//     {
-//         const li = document.createElement("li");
-//         li.innerHTML = item;
-//         listElement.appendChild(li);
-//     }
-// }
-
-/**
- * Arrow
- * @param {Element} listElement 
- * @param {Array} array 
- */
-let createList = (listElement, array = ["puppydogs", "butterflies", "lollipops"]) =>
+let init = function()
 {
-    for(let item of array)
-    {
-        const li = document.createElement("li");
-        li.innerHTML = item;
-        listElement.appendChild(li);
-    }
+    
 }
-
-
-
-createList(document.querySelector("#colorsList"), colors);
-createList(document.querySelector("#foodsList"), foods);
-
-
-const linksOrderedList = document.querySelector("#linksList");
-for(let link in links)
-{
-    const li = document.createElement("li");
-    li.innerHTML = `<a href="${links[link]}">${link}</a>`;
-    linksOrderedList.appendChild(li);
-}
-
-
-
