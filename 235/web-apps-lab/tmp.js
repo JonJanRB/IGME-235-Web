88e330816264e212fba827cec39fdc6d6828b666
Jonathan Jan (RIT Student) <jmj2097@rit.edu>
2023-10-31 11:57:19 -0400

    started exercise 5, finished exercise 6

--- 235/web-apps-lab/tmp.js
+++ 235/web-apps-lab/tmp.js
@@ -4,11 +4,22 @@
 // attribute selector: input[name='colorGroup']
 // get all the things (legend, radios, btn, para)
 
+let init = function()
+{
+    let buttons = document.querySelectorAll("input");
+    for(let button of buttons)
+    {
+        button.onclick = selection;
+    }
+}
+
 // JQuery was made by a cs student at RIT !?
 window.onload = init;
 // window.addEventListener("load", init); //Another way to do this
 
-let init = function()
+
+
+let selection = colorName =>
 {
-    
+    console.log(colorName);
 }
