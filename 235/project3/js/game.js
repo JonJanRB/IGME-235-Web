38a25188b601b6ec8e8c9c9c80ea47046f091574
Jonathan Jan <jjanrb@gmail.com>
2023-12-15 06:45:33 -0500

    started classes

--- 235/project3/js/game.js
+++ 235/project3/js/game.js
@@ -1,5 +1,49 @@
-// import * as PIXI from './pixi.js';
+import * as PIXI from './lib/pixi.js';
+import * as Victor from './lib/victor.js';
+
 "use strict";
+
+//#region Classes
+
+class PhysicsObject extends PIXI.Graphics
+{
+    constructor(radius, color = 0xFF0000, x = 0, y = 0)
+    {
+        super();
+
+        //Properties (I love not having types (that is sarcasm))
+        /**@type {number}*/this.color = color;
+        //Collider
+        /**@type {number}*/this.colliderRadius = 1;
+        // /**@type {number}*/this.rotation = 0;
+
+        this.beginFill(color);
+        this.drawCircle(0, 0, radius);
+        this.endFill();
+        this.radius = radius;
+        this.fwd = getRandomUnitVector();
+        this.speed = 50;
+        this.isAlive = true;
+    }
+
+    //"Properties"
+    /**
+     * Returns the position as a vector
+     * @returns {Victor}
+     */
+    getPosition() { return Victor(super.x, super.y); }
+    /**
+     * Sets the position of this object
+     * @param {Victor} position 
+     */
+    setPosition(position)
+    {
+        super.x = position.x;
+        super.y = position.y;
+    }
+}
+
+//#endregion
 
 //#region Global Variables
 
@@ -12,6 +56,8 @@ let ASSETS;
 
 
 //#endregion
+
+//#region Initialization
 
 /**
  * Initial method for loading into the DOM
@@ -35,7 +81,7 @@ const loadAssets = async() =>
     //     explosions: 'images/explosions.png',
     //     move: 'images/move.png'
     //   });
-    ASSETS = await PIXI.Assets.loadBundle('sprites');
+    // ASSETS = await PIXI.Assets.loadBundle('sprites');
     start();
 }
 
@@ -46,3 +92,5 @@ const start = () =>
 {
     STAGE.addChild(new PIXI.Graphics().beginFill(0xff0000).drawRect(0, 0, APP_WIDTH/2, APP_HEIGHT/2));
 }
+
+//#endregion
