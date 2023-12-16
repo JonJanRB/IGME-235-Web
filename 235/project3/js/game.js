d910e208b7b6cb353b792c679a48bacb20f4288a
Jonathan Jan <jjanrb@gmail.com>
2023-12-15 22:12:25 -0500

    backup before trying to implement camera

--- 235/project3/js/game.js
+++ 235/project3/js/game.js
@@ -1,6 +1,7 @@
 //These are used for the intellisense and then commented out to run
 // import * as PIXI from './lib/pixi.js';
 // import * as Victor from './lib/victor.js';
+// import * as viewport from './lib/pixi_viewport.js';
 
 "use strict";
 
@@ -81,7 +82,7 @@ class Orb extends PhysicsObject
      * @param {number} colliderRadius The radius of the circle collider
      * @param {number} color The color of the circle
      */
-    constructor(visualRadius, colliderRadius, color = 0x00ff00)
+    constructor(visualRadius, colliderRadius = visualRadius*2, color = 0x00ff00)
     {
         //Call the base constructor where it draws a circle
         super(phys =>
@@ -97,8 +98,40 @@ class Orb extends PhysicsObject
         //Add to the list of orbs
         ORBS.push(this);
     }
+}
 
-    //I would like to add particles too
+class Player extends PhysicsObject
+{
+    constructor(scaleAmount = 100, colliderRadius = scaleAmount*0.5, color = 0x0000ff)
+    {
+        super(phys =>
+        {
+            phys.beginFill(color);
+            const negativeHalfScale = scaleAmount*-0.5;
+            phys.drawRect(-negativeHalfScale, -negativeHalfScale, scaleAmount, scaleAmount);
+            phys.endFill();
+        });
+
+        super.colliderRadius = colliderRadius;
+    }
+}
+
+class Spike extends PhysicsObject
+{
+    constructor(scaleAmount, colliderRadius = scaleAmount*0.2, color = 0x0000ff)
+    {
+        super(phys =>
+        {
+            const halfScale = scaleAmount*0.5;
+            phys.beginFill(color);
+            phys.lineTo(-halfScale, halfScale);
+            phys.lineTo(halfScale, halfScale);
+            phys.lineTo(0, -halfScale);
+            phys.endFill();
+        });
+
+        super.colliderRadius = colliderRadius;
+    }
 }
 
 //#endregion
@@ -144,7 +177,7 @@ const init = () =>
 {
     //Add the game panel to the dom
     document.body.appendChild(APP.view);
-
+    // viewport.create(createRenderer());
     loadAssets();
 }
 window.onload = init;
@@ -163,13 +196,44 @@ const loadAssets = async() =>
     start();
 }
 
+//https://davidfig.github.io/pixi-viewport/ These helped for camera
+//https://davidfig.github.io/pixi-viewport/jsdoc/Viewport.html
+// const createRenderer = () => 
+// {
+//     const renderer = new PIXI.Renderer(
+//     {
+//         backgroundAlpha: 0,
+//         width: window.innerWidth,
+//         height: window.innerHeight,
+//         resolution: window.devicePixelRatio,
+//         antialias: true,
+//     });
+//     document.body.appendChild(renderer.view);
+//     return renderer;
+//     // renderer.view.style.position = 'fixed';
+//     // renderer.view.style.width = '100vw';
+//     // renderer.view.style.height = '100vh';
+//     // renderer.view.style.top = 0;
+//     // renderer.view.style.left = 0;
+//     // renderer.view.style.background = 'rgba(0,0,0,.1)';
+// }
+
 /**
  * Starts the game 
  */
 const start = () =>
 {
-    const tmp = new Orb(5, 5);
-    STAGE.addChild(tmp);
+    const orb = new Orb(50);
+    orb.vectorPosition = Victor(APP_WIDTH*0.5, APP_HEIGHT*0.5);
+    STAGE.addChild(orb);
+
+    const player = new Player(20);
+    player.vectorPosition = Victor(30, 50);
+    STAGE.addChild(player);
+
+    const spike = new Spike(50);
+    spike.vectorPosition = Victor(APP_WIDTH*0.5 + 100, APP_HEIGHT*0.5 + 100);
+    STAGE.addChild(spike);
 
     //Begin the game loop
     APP.ticker.add(update);
@@ -187,3 +251,4 @@ const update = () =>
 }
 
 //#endregion
+
